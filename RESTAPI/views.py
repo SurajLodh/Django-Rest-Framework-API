a150b0d3cede29a80e4cb80654208824ac9decf6
uday Gupta <uday.gupta@exponentia.ai>
2021-11-01 14:36:13 +0530

    user register api added

--- RESTAPI/views.py
+++ RESTAPI/views.py
@@ -1,11 +1,16 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from .models import Student
-from .serializers import StudentSerializer
-from rest_framework import viewsets
+from django.contrib.auth.models import User
+from .serializers import *
+from rest_framework import viewsets, generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 
 class StudentViewSet(viewsets.ModelViewSet):
@@ -15,3 +20,9 @@ class StudentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
     
+
+@method_decorator(csrf_exempt, name='dispatch') 
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
